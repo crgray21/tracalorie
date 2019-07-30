bb0f40333cd0f9642acfa31bba191a690b74d470
Gray, Cory (Genworth) <Cory.Gray@genworth.com>
2019-07-30 16:06:23 -0400

    Clear and delete from local storage

--- app.js
+++ app.js
@@ -23,8 +23,29 @@ const StorageCtrl = (function() {
                 items = JSON.parse(localStorage.getItem('items'));
             }
             return items;
+        },
+        updateItemStorage: function(updatedItem) {
+            let items = JSON.parse(localStorage.getItem('items'));
+            items.forEach((item, index) => {
+                if (updatedItem.id === item.id) {
+                    items.splice(index, 1, updatedItem);
+                }
+            });
+            localStorage.setItem('items', JSON.stringify(items));
+        },
+        deleteItemFromStorage: function(id) {
+            let items = JSON.parse(localStorage.getItem('items'));
+            items.forEach((item, index) => {
+                if (id === item.id) {
+                    items.splice(index, 1);
+                }
+            });
+            localStorage.setItem('items', JSON.stringify(items));
+        },
+        clearItemsFromStorage: function() {
+            localStorage.removeItem('items');
         }
-    }
+    };
 
 })();
 
@@ -254,6 +275,7 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
         UICtrl.updateListItem(updatedItem);
         const totalCalories = ItemCtrl.getTotalCalories();
         UICtrl.showTotalCalories(totalCalories);
+        StorageCtrl.updateItemStorage(updatedItem);
         UICtrl.clearEditState();
 
         e.preventDefault();
@@ -266,6 +288,7 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
 
         const totalCalories = ItemCtrl.getTotalCalories();
         UICtrl.showTotalCalories(totalCalories);
+        StorageCtrl.deleteItemFromStorage(currentItem.id);
         UICtrl.clearEditState();
 
         e.preventDefault();
@@ -289,6 +312,7 @@ const App = (function(ItemCtrl, StorageCtrl, UICtrl) {
         const totalCalories = ItemCtrl.getTotalCalories();
         UICtrl.showTotalCalories(totalCalories);
         UICtrl.removeItems();
+        StorageCtrl.clearItemsFromStorage();
         UICtrl.hideList();
     }
 
